afe423a4e956649e30481a8dcf3a1b47929f944f
Tsvetelinna <33135589+Tsvetelinna@users.noreply.github.com>
2018-01-04 22:16:40 +0200

    4zad

--- homework2/4zad.cpp
+++ homework2/4zad.cpp
@@ -1,70 +1,75 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 
 int main()
 {
 	const int size = 105;
 	int arr[size][2];
 	int N = 0;
+
+	cout << "Input number between 5 and 105." << endl;
 	cin >> N;
 
-
-	if (N >= 1 && N <= 105)
+	if (N < 5 || N > 105)
 	{
-		for (int i = 0; i < N; i++)
+		do
 		{
-			for (int j = 0; j < 2; j++)
+			cout << "Input number between 5 and 105." << endl;
+			cin >> N;
+		} while (N < 5 || N > 105);
+
+	}
+	
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			int num;
+			cin >> num;
+
+			if (num >= 10 && num <= 99)
 			{
-				int num;
-				cin >> num;
-				if (num >= 10 && num <= 99)
-				{
-					arr[i][j] = num;
-				}
-				else
-				{
-					j--;
-				}
+				arr[i][j] = num;
+			}
+
+			else
+			{
+				j--;
+			}
+
 				cout << endl;
-			}
 		}
-
-		for (int i = 0; i < N; i++)
-		{
-			int percent;
-
-			int numerator;
-			int denominator;
-
-			for (int j = 0; j < 2; j++)
-			{
-				if (j == 0)
-				{
-					numerator = arr[i][j];
-					cout << numerator << " / ";
-				}
-				if (j == 1)
-				{
-					denominator = arr[i][j];
-					cout << denominator << " = ";
-				}
-				
-
-				percent= (numerator * 100 / denominator) ;
-			}
-
-
-			cout << percent << " % " << endl;
-		}
-
 	}
 
-
-	else
+	for (int i = 0; i < N; i++)
 	{
-		cout << "Error";
+		int percent;
+		int numerator;
+		int denominator;
 
+		for (int j = 0; j < 2; j++)
+		{
+			if (j == 0)
+			{
+				numerator = arr[i][j];
+				cout << numerator << " / ";
+			}
+
+			if (j == 1)
+			{
+				denominator = arr[i][j];
+				cout << denominator << " = ";
+			}
+
+			percent = (numerator * 100 / denominator);
+		}
+
+		cout << percent << " % " << endl;
 	}
+
+
+
 
 	system("Pause");
 
