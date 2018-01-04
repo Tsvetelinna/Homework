bb65ea790a644b295879c76e4bad990eb3a454de
Tsvetelinna <33135589+Tsvetelinna@users.noreply.github.com>
2018-01-04 22:26:30 +0200

    9zad

--- homework2/9zad.cpp
+++ homework2/9zad.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 bool isCorrect(int num)
 {
-	int temp=num;
-	int sum=0;
-	while(num>0)
+	int temp = num;
+	int sum = 0;
+	while (num>0)
 	{
-		sum+=num%10;
-		num/=10;
+		sum += num % 10;
+		num /= 10;
 	}
 
-	if (temp%sum==0)
+	if (temp%sum == 0)
 	{
 		return true;
 	}
@@ -23,11 +23,23 @@ bool isCorrect(int num)
 int main()
 {
 	int number;
-	cin>>number;
 
-	cout<<boolalpha<<isCorrect(number);
+	cout << "Input number between 111 and 22222." << endl;
+	cin >> number;
 
-	
+	if (number < 111 || number > 22222)
+	{
+		do
+		{
+			cout << "Input number between 111 and 22222." << endl;
+			cin >> number;
+		} while (number < 111 || number > 22222);
+
+	}
+
+	cout << boolalpha << isCorrect(number);
+
+
 
 	return 0;
 }
