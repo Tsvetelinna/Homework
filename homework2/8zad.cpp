7a88f336c9dd58ca39c258cb704bae9773cdd9fe
Tsvetelinna <33135589+Tsvetelinna@users.noreply.github.com>
2018-01-04 22:21:40 +0200

    8zad

--- homework2/8zad.cpp
+++ homework2/8zad.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	int num;
-	cin>>num;
+	int fib[40];
+	fib[0] = 1;
+	fib[1] = 1;
 
-	int fib[41];
-	fib[0]=1;
-	fib[1]=1;
+	cout << "Input number between 1 and 40." << endl;
+	cin >> num;
 
-	for (int i=2; i<=num; i++)
+	if (num < 1 || num > 40)
 	{
-		fib[i]=fib[i-1]+fib[i-2];		
-				
+		do
+		{
+			cout << "Input number between 1 and 40." << endl;
+			cin >> num;
+		} while (num < 1 || num > 40);
+
+	}
+	
+	
+	for (int i = 2; i <= num; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+
 	}
 
-	cout<<fib[num];
+	cout << fib[num]<<endl;
 
+
+	system("Pause");
 
 	return 0;
 }
+
 
