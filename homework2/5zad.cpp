ff9fbf574b49c486fc070f4cb9392ed05e02fdf5
Tsvetelinna <33135589+Tsvetelinna@users.noreply.github.com>
2018-01-04 18:43:45 +0200

    5zad

---
+++ homework2/5zad.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int num;
+	int numberofzeros;
+	int bin[21]; //Числото един милион в двуичен запис съдържа 20 позиции, от което следва, че няма да се събере в числен тип. Затова ще използвам масив от 20 елемента.
+	int count = 0;
+	int maxzeros = 0;
+	int current = 0;
+
+	cout << "Input number between 1000 and 1 000 000.";
+	cin >> num;
+
+	if (num < 1000 || num > 1000000)
+	{
+		do
+		{
+			cout << "Input number between 1000 and 1 000 000." << endl;
+			cin >> num;
+		} while (num < 1000 || num > 1000000);
+
+	}
+
+
+	cin >> numberofzeros;
+
+	while (num > 0)
+	{
+		bin[count] = num % 2;
+		num /= 2;
+		count++;
+	}
+
+
+	for (int i = 0; i<count; i++)
+		{
+			if (bin[i] == 0)
+			{
+				current++;
+			}
+			else
+			{
+				if (current > maxzeros)
+				{
+					maxzeros = current;
+				}
+
+				current = 0;
+			}
+		}
+
+		if (maxzeros == numberofzeros || maxzeros>numberofzeros)
+		{
+			cout << "Yes" << endl;
+		}
+		else
+		{
+			cout << "No" << endl;
+		}
+	
+
+	
+	return 0;
+}
